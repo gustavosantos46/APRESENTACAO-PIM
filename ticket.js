dc0248c0bb7dedfadd0b6d9df3d41f8368aea13b
Gustavo Silva <122186116+gustavosantos46@users.noreply.github.com>
2025-10-07 13:36:59 -0300

    create ticket.js

---
+++ ticket.js
@@ -0,0 +1,7 @@
+document.querySelector(".attachment-btn").addEventListener("click", () => {
+  alert("Abrindo anexo recebido...");
+});
+
+document.querySelector(".start-btn").addEventListener("click", () => {
+  alert("Atendimento iniciado com sucesso!");
+});
